7470e0be47093c00d69ba3b247103a2c089f5783
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2023-12-20 21:52:49 +0500

    джаваскрипт

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -43,3 +43,49 @@ const init = () => {
 };
 
 window.addEventListener('DOMContentLoaded', init);
+
+
+const sliderItems = Array.from(document.querySelectorAll('.hero-list__item'));
+const sliderPagination = Array.from(document.querySelectorAll('.hero-pagination__button'));
+const sliderPrev = document.querySelector('.slider-prev');
+const sliderNext = document.querySelector('.slider-next');
+
+sliderNext.addEventListener('click', () => {
+  const index = sliderItems.findIndex((item) => item.classList.contains('hero-list__item--active'));
+  if (index === sliderItems.length - 1) {
+    sliderNext.setAttribute('disabled', 'true');
+  } else {
+    sliderNext.removeAttribute('disabled');
+    sliderPrev.removeAttribute('disabled');
+    sliderItems[index].classList.remove('hero-list__item--active');
+    sliderItems[index + 1].classList.add('hero-list__item--active');
+    sliderPagination[index].classList.remove('hero-pagination__button-active');
+    sliderPagination[index + 1].classList.add('hero-pagination__button-active');
+  }
+});
+
+sliderPrev.addEventListener('click', () => {
+  const index = sliderItems.findIndex((item) => item.classList.contains('hero-list__item--active'));
+  if (index === 0) {
+    sliderPrev.setAttribute('disabled', 'true');
+  } else {
+    sliderNext.removeAttribute('disabled');
+    sliderPrev.removeAttribute('disabled');
+    sliderItems[index].classList.remove('hero-list__item--active');
+    sliderItems[index - 1].classList.add('hero-list__item--active');
+    sliderPagination[index].classList.remove('hero-pagination__button-active');
+    sliderPagination[index - 1].classList.add('hero-pagination__button-active');
+  }
+});
+
+const mobileMenu = document.querySelector('.mobile-nav');
+const mobileIcon = document.querySelector('.mobile-nav__icon--close');
+const siteList = document.querySelector('.site-list');
+
+mobileMenu.classList.remove('mobile-nav--nojs');
+siteList.classList.remove('site-list--nojs');
+
+mobileMenu.addEventListener('click', () => {
+  siteList.classList.toggle('site-list--open');
+  mobileIcon.classList.toggle('mobile-nav__icon--close');
+});
